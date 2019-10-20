567e2adf0888df45aa7372f91615cee8beb2f2bf
Willehard Korander <willehard@gmail.com>
2019-10-20 16:52:23 +0300

    Fixed ft_memmove.

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 14:26:28 by wkorande          #+#    #+#             */
-/*   Updated: 2019/10/20 16:48:20 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/10/20 16:50:27 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	unsigned char	*s;
 	size_t			i;
 
-	if (!src || !dst)
+	if (!src && !dst)
 		return (NULL);
 	d = (unsigned char*)dst;
 	s = (unsigned char*)src;
