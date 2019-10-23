f3026bed1c0fe4e64ef804d84b22231a0d5ab989
Willehard Korander <willehard@gmail.com>
2019-10-23 16:40:34 +0300

    Fixed bug. Oooops.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,10 +6,11 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 17:15:53 by wkorande          #+#    #+#             */
-/*   Updated: 2019/10/17 17:21:29 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/10/23 16:39:28 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
@@ -19,9 +20,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2 + 1);
-	if (!(join = (char*)ft_memalloc(len + 1)))
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(join = (char*)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
+	ft_bzero(join, len + 1);
 	join = ft_strcat(join, (char*)s1);
 	join = ft_strcat(join, (char*)s2);
 	return (join);
