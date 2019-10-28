e90ef0e79d1af004f42f0486f96b1e7fbbd3ae0d
Willehard Korander <willehard@gmail.com>
2019-10-28 17:30:19 +0200

    Some tweaks to itoa base.

--- srcs/ft_itoa_base.c
+++ srcs/ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 14:46:08 by wkorande          #+#    #+#             */
-/*   Updated: 2019/10/28 12:04:38 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/10/28 17:30:01 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@ char	*ft_itoa_base(int n, char *digits)
 	unsigned int	base;
 
 	base = ft_strlen(digits);
-
-	if (n < 0)
-		nbr = (unsigned int)(n * -1);
+	if (n < 0 && base == 10)
+		nbr = (unsigned int)(-n);
 	else
 		nbr = (unsigned int)n;
-	len = (unsigned int)ft_ndigits_base(n, base) + (n < 0);
+	len = (unsigned int)ft_ndigits_base(nbr, base) + (n < 0);
 	if (!(str = (char*)malloc(sizeof(char) * len + 1)))
 		return (NULL);
-	str[len] = '\0';
-	while (len--)
+	str[len--] = '\0';
+	while (nbr != 0)
 	{
 		str[len] = digits[nbr % base];
 		nbr /= base;
+		len--;
 	}
 	if (n < 0)
 		str[0] = '-';
