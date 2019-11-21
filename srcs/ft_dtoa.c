6aa3f82cae2fa5775031d2b1aea6b4d5b63a91af
Willehard Korander <willehard@gmail.com>
2019-11-21 13:11:56 +0200

    dtoa seems to work. ugly precision point logic

--- srcs/ft_dtoa.c
+++ srcs/ft_dtoa.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 10:36:01 by wkorande          #+#    #+#             */
-/*   Updated: 2019/11/21 12:54:18 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/11/21 13:10:22 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,9 @@ static char	*ft_fractoa(double d, int precision)
 	i = 0;
 	while (precision--)
 	{
-		nbr *= 10;
+		nbr *= 10.0;
+		if (precision == 0)
+			nbr += 0.5;
 		str[i++] = ((int)nbr % 10) + '0';
 	}
 	return (str);
@@ -41,13 +43,15 @@ char		*ft_dtoa(double d, int precision)
 	char	*str;
 	char	*ds;
 	char	*fs;
+	char	*t;
 
 	if (!precision)
 		return (ft_itoa((int)d));
 	ds = ft_itoa((int)d);
 	fs = ft_fractoa(d, precision);
-	ds = ft_strjoin(ds, ".");
-	str = ft_strjoin(ds, fs);
+	t = ft_strjoin(ds, ".");
+	str = ft_strjoin(t, fs);
+	free(t);
 	free(ds);
 	free(fs);
 	return (str);
