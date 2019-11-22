50070591b358973b574d965c40e965435a294327
Willehard Korander <willehard@gmail.com>
2019-11-22 16:15:09 +0200

    Added function

--- srcs/ft_itoa_base_uint64.c
+++ srcs/ft_itoa_base_uint64.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 14:46:08 by wkorande          #+#    #+#             */
-/*   Updated: 2019/11/22 13:50:26 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/11/22 15:18:18 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_itoa_base_uint64(uint64_t n, char *digits)
 	size_t	len;
 	size_t	base;
 
+	if (n == 0)
+		return (ft_strdup("0"));
 	base = ft_strlen(digits);
 	len = (size_t)ft_ndigits_base_uint64(n, base);
 	if (!(str = (char*)malloc(sizeof(char) * len + 1)))
