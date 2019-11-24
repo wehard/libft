921abbe95e47c570ae925568360b60421ed6b07f
Willehard Korander <willehard@gmail.com>
2019-11-24 12:07:58 +0200

    Type fix

--- srcs/ft_ndigits_base_uint64.c
+++ srcs/ft_ndigits_base_uint64.c
@@ -6,13 +6,13 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 12:22:51 by wkorande          #+#    #+#             */
-/*   Updated: 2019/11/22 13:47:50 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/11/24 12:06:39 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_ndigits_base_uint64(u_int64_t n, int base)
+int	ft_ndigits_base_uint64(uint64_t n, int base)
 {
 	int i;
 
