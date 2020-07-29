4b55b2bd46c254b3fba1aed11876826c9090c8cb
Willehard Korander <willehard@gmail.com>
2020-07-29 22:15:25 +0300

    fix div

--- srcs/color/ft_div_rgba.c
+++ srcs/color/ft_div_rgba.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/29 22:06:21 by wkorande          #+#    #+#             */
-/*   Updated: 2020/07/29 22:07:37 by wkorande         ###   ########.fr       */
+/*   Updated: 2020/07/29 22:12:55 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_rgba	ft_div_rgba(t_rgba c, double t)
 {
-	if (t == 0)
+	if (t == 0.0)
 		return (c);
 	c.r /= t;
 	c.g /= t;
